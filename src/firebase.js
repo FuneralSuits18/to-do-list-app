17f30b36ef42ada9914ca93430304607c47f20e0
FuneralSuits18 <50417827+FuneralSuits18@users.noreply.github.com>
2023-04-19 17:00:52 +0600

    Add Firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDV2baezy09YizI-IP43HWLM9RM-2qJxzs',
+  authDomain: 'todo-app-dc17a.firebaseapp.com',
+  projectId: 'todo-app-dc17a',
+  storageBucket: 'todo-app-dc17a.appspot.com',
+  messagingSenderId: '981831796551',
+  appId: '1:981831796551:web:8c7a1ec7ba92c992022e62',
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+export {app};
